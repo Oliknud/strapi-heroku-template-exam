6d2c857f7e344d43ba1e310dd7d174445686e2d8
Oliknud <69842780+Oliknud@users.noreply.github.com>
2022-04-25 22:45:25 +0200

    .

--- src/api/hotel/services/hotel.js
+++ src/api/hotel/services/hotel.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test service.
+ * hotel service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::test.test');
+module.exports = createCoreService('api::hotel.hotel');
